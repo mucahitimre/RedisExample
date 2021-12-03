53f08ca89c85f03aeb17b8bcf2516d06d040e60c
Mücahit imre <mucahit.imre@gmail.com>
2021-12-04 02:14:17 +0300

    Add project files.

---
+++ RedisExample/Controllers/RedisController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RedisExample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RedisController : ControllerBase
+    {
+        public RedisController()
+        {
+        }
+
+
+    }
+}
