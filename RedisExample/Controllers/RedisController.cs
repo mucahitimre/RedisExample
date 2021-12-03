0374ed5ceb13e46b38ec476343ad9948a14a86eb
Mücahit imre <mucahit.imre@gmail.com>
2021-12-04 02:35:35 +0300

    refactoring

--- RedisExample/Controllers/RedisController.cs
+++ RedisExample/Controllers/RedisController.cs
@@ -1,9 +1,15 @@
 ﻿namespace RedisExample.Controllers
 {
+    /// <summary>
+    /// The redis controller
+    /// </summary>
     [Route("api/[controller]")]
     [ApiController]
     public class RedisController : ControllerBase
     {
+        /// <summary>
+        /// The redis controller
+        /// </summary>
         public RedisController()
         {
         }
